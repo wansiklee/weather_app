8cb639e53e058e45b6b77c93841b3fdb6e337bfd
wansiklee <dhkstlr2662@gmail.com>
2019-10-23 15:40:45 +0900

    [update]getting the location & asking for permission

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("위치를 찾을 수 없어요ㅠㅠ", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
